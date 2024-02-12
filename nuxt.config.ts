1d3b149b5aad2629dae89be12771f44ef731e0bb
danielcrubens <dcr.tecnologi@gmail.com>
2024-02-12 17:03:23 -0300

    configurações adicionais  :white_check_mark:

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
+  app: {
+    head: {
+      title: 'Kotas - Pokedex',
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1',
+          charset: 'utf-8'
+        }
+      ],
+    }
+    },
   modules: [
     '@vueuse/nuxt',
     '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
   ],
   components: true,
-
   css:
-    [
-      '@/assets/css/tailwind.css',
-      '@/assets/css/global.css',
-    ],
+  [
+    '@/assets/css/tailwind.css',
+    '@/assets/css/global.css',
+  ],
+  pinia: {
+    storesDirs: ['./stores/**']
+  },
+  
 })
